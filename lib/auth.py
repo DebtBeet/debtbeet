92a8580c5777ea184d9b0161898c5c5340c133a1
Japhy <github@pearachute.com>
2012-06-02 17:19:16 -0400

    working signup/login

--- lib/auth.py
+++ lib/auth.py
@@ -1,6 +1,6 @@
 import tornado
 import hashlib
-from logging import info
+from logging import info, warning
 
 class AuthHandler( tornado.web.RequestHandler):
     def get_current_user(self):
@@ -18,6 +18,7 @@ class Logout( AuthHandler):
 class Login( AuthHandler):
     def get( self, errormsg=None):
         next = self.get_argument("next", None)
+        if errormsg: warning( errormsg)
         self.render( 'login.html', next=next, errormsg=errormsg)
 
     def post( self):
@@ -25,18 +26,23 @@ class Login( AuthHandler):
         password = self.get_argument( 'password', None)
 
         if not password or not login:
+            warning('didnt fill required fields')
             return self.get( "please enter both a name and a password")
 
-        userobj = M[db].users.find_one( {'user':login.lower()})
-        if not userobj: return self.get( "Invalid login or password!")
+        userobj = self.application.M.auth.find_one( {'login':login.lower()})
+        if not userobj: 
+            warning('user doesnt exist')
+            return self.get( "Invalid login or password!")
 
-        if not sha512( password).hexdigest() == userobj['passhash']:
+        if not hashlib.sha256( password).hexdigest() == userobj['password']:
+            warning('bad password')
             return self.get( "Invalid login or password!" )
 
         self.set_secure_cookie("deadbeetuser", login)
 
         nexturl = self.get_argument('next', None)
         self.redirect( nexturl or '/')
+
 
 class Signup( AuthHandler):
     def get( self, errormsg=None):
@@ -56,17 +62,15 @@ class Signup( AuthHandler):
         if not user:
             return self.mkuser( login=login,password=password,phone=phone,email=email)
 
-        #check password
-        if user['password'] != password: return self.get( errormsg='Wrong Password')
 
         else:
-            nxt = self.get_argument('next', '/')
-            self.redirect( nxt)
-            
+            info( 'User exists, redirect to login')
+            self.redirect( '/login') #user already exists
+           
+ 
     def mkuser(self, **kwarg):
         self.application.M.auth.insert( dict(**kwarg))
-
-
-
+        info( 'User created')
+        return self.redirect('/')
 
 
