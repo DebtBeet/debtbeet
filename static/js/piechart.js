ad2de3dfd7259fd0631d8d58df7a4bd70688403c
trinachi <trinachi@gmail.com>
2012-06-02 22:42:45 -0500

    change dollar amounts in input fields

--- static/js/piechart.js
+++ static/js/piechart.js
@@ -8,7 +8,7 @@ function pieChart() {
 
     r.customAttributes.segment = function (x, y, r, a1, a2) {
         var flag = (a2 - a1) > 180,
-            clr = (a2 - a1) / 360; // color the segments
+            clr = .23; // color the segments
         a1 = (a1 % 360) * Math.PI / 180;
         a2 = (a2 % 360) * Math.PI / 180;
         return {
@@ -26,15 +26,13 @@ function pieChart() {
             paths[i].angle = start - val / 2;
         }
     }
-	
-	var input1 = parseInt($('#input1').val());
-	var input2 = parseInt($('#input2').val());
-	var input3 = parseInt($('#input3').val());
-	var input4 = parseInt($('#input4').val());
-	var input5 = parseInt($('#input5').val());
 
-    var data = [input1, input2, input3, input4, input5],
-        paths = r.set(),
+    var data = [];
+		var inputs = $('#beets').children('.amount').length;
+			for (var i = 0; i < inputs; i++) {
+				data.push(parseInt($('.amount:nth-child(' + (i+1) + ')').val()));
+			}
+    var paths = r.set(),
         total,
         start,
         bg = r.circle(200, 200, 0).attr({stroke: "#fff", "stroke-width": 4});
